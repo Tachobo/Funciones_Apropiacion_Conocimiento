a50a05458207cd61b6b72f4b72502d11e682cbb8
Tachobo <wilmerardila2001@gmail.com>
2025-11-08 17:47:28 -0500

    tercer commit: contarhasta

---
+++ app2.js
@@ -0,0 +1,19 @@
+// function contarHasta (numero){
+//     for (let i = 1; i <= numero; i++){
+//         console.log(i);
+//     }
+// }
+// let numeroEntrante = prompt("Por favor digite un numero");
+// let numero = parseInt(numeroEntrante);
+
+// contarHasta(numero);
+
+const contarHasta = (numero)=>{
+    for (let i = 1; i <= numero; i++){
+        console.log(i);
+    }
+}
+let numeroEntrante = prompt("Por favor digite un numero");
+let numero = parseInt(numeroEntrante);
+
+contarHasta(numero);
